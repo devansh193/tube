49057c3f3a561a53762c6cd3c67a3295bf863f9e
devansh <devanshverma193@gmail.com>
2025-02-10 23:45:19 +0530

    trpc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { HydrateClient, trpc } from "@/trpc/server";
 import { PageClient } from "./client";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
   void trpc.hello.prefetch({ text: "Devansh" });
@@ -8,7 +9,9 @@ export default async function Home() {
   return (
     <HydrateClient>
       <Suspense fallback={<p>Loading....</p>}>
-        <PageClient />
+        <ErrorBoundary fallback={<p>Error....</p>}>
+          <PageClient />
+        </ErrorBoundary>
       </Suspense>
     </HydrateClient>
   );
