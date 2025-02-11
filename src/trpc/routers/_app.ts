4243dfd795ae9fe63c0e913747a86c611464d4bb
devansh <devanshverma193@gmail.com>
2025-02-11 12:49:19 +0530

    trpc

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
     .query((opts) => {
+      console.log({ dBUSer: opts.ctx.clerkUserId });
       return {
         greeting: `hello ${opts.input.text}`,
       };
     }),
 });
-// export type definition of API
 export type AppRouter = typeof appRouter;
