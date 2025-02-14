0c96ee3bfd71937574180ccc353954ba6401fafc
devansh <devanshverma193@gmail.com>
2025-02-14 16:06:15 +0530

    mux

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -50,6 +50,27 @@ export const POST = async (request: Request) => {
         .where(eq(videos.muxUploadId, data.upload_id));
       break;
     }
+    case "video.asset.ready": {
+      const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+      const playbackId = data.playback_ids?.[0].id;
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 });
+      }
+      if (!playbackId) {
+        return new Response("Missing playback Id", { status: 400 });
+      }
+      const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status,
+          muxPlaybackId: playbackId,
+          musAssetId: data.id,
+          thumbnailUrl,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+      break;
+    }
   }
   return new Response("Webhook received", { status: 200 });
 };
